ea208d94e4c7cea6ebaee9849fc940a55b8b12b7
lifanica <ivan.lifanica@gmail.com>
2023-02-15 20:04:34 +0200

    new algs

--- codingQuestions/src/questions/PrimeNumbers.java
+++ codingQuestions/src/questions/PrimeNumbers.java
@@ -4,10 +4,13 @@ package questions;
  * @author Ivan Lifanica
  */
 public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println(isPrime(10));
+    }
 
-    public boolean isPrime(int number){
+    static public boolean isPrime(int number){
         if(number == 1) return false;
-        for(int i = 2; i<Math.sqrt(number); i++){
+        for(int i = 2; i< number >>> 1 ; i++){
             if(number % i == 0) {
                 return false;
             }
